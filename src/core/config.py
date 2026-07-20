2052b0a95aa045c639ede2f8374691befc5c6bbc
shivraj.dev <timilsenashivraj598@gmail.com>
2026-07-20 18:30:29 +0545

    Refactor config: remove unused GEMINI_API_KEY and related environment variable setting

--- src/core/config.py
+++ src/core/config.py
@@ -12,7 +12,6 @@ load_dotenv()
 class Settings:
     """Application settings loaded from environment variables."""
 
-    GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
     TAVILY_API_KEY: str = os.getenv("TAVILY_API_KEY", "")
     CLOUDFLARE_API_KEY: str = os.getenv("CLOUDFLARE_API_KEY", "")
     CLOUDFLARE_ACCOUNT_ID: str = os.getenv("CLOUDFLARE_ACCOUNT_ID", "")
@@ -22,5 +21,4 @@ settings = Settings()
 
 # Set env variables for LangChain integrations
 os.environ["TAVILY_API_KEY"] = settings.TAVILY_API_KEY
-os.environ["GOOGLE_API_KEY"] = settings.GEMINI_API_KEY
 os.environ["OPENAI_API_KEY"] = settings.CLOUDFLARE_API_KEY
