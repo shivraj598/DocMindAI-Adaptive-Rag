1617234862db4e2e1fd22a5ef35da6a1866d24dc
shivraj.dev <timilsenashivraj598@gmail.com>
2026-07-20 17:47:08 +0545

    Add Cloudflare API keys to settings and environment variables

--- src/core/config.py
+++ src/core/config.py
@@ -14,6 +14,8 @@ class Settings:
 
     GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
     TAVILY_API_KEY: str = os.getenv("TAVILY_API_KEY", "")
+    CLOUDFLARE_API_KEY: str = os.getenv("CLOUDFLARE_API_KEY", "")
+    CLOUDFLARE_ACCOUNT_ID: str = os.getenv("CLOUDFLARE_ACCOUNT_ID", "")
 
 
 settings = Settings()
@@ -21,3 +23,4 @@ settings = Settings()
 # Set env variables for LangChain integrations
 os.environ["TAVILY_API_KEY"] = settings.TAVILY_API_KEY
 os.environ["GOOGLE_API_KEY"] = settings.GEMINI_API_KEY
+os.environ["OPENAI_API_KEY"] = settings.CLOUDFLARE_API_KEY
